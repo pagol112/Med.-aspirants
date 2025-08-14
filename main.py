e47f762808e67bd5de533809e9a993134e760e4e
Shishir <tui@squad.com>
2025-08-14 15:30:58 +0600

    Squad sync before rebase

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class SquadApp(App):
+    def build(self):
+        return Label(text="🔥 Squad APK Build Successful!")
+
+if __name__ == "__main__":
+    SquadApp().run()
 
